d296c0dd50d0bcbbf2141cae0dc9e116d48d7e38
natashabusnardo <natasha.busnardo@mercadolivre.com>
2022-09-22 17:11:05 -0300

    example02

---
+++ Modulo2Java/Aula01/src/examples/example02.java
@@ -0,0 +1,18 @@
+package examples;
+
+public class example02 {
+    public static void main(String[] args) {
+
+        int numero = 0;
+
+        if (numero > 0) {
+            System.out.println("O valor é positivo");
+        } else {
+            if (numero < 0){
+                System.out.println("O valor é negativo");
+            } else {
+                System.out.println("O valor é zero");
+            }
+        }
+    }
+}
