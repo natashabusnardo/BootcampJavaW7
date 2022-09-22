7c857e28b2f27df639b596cb51e0c7f27eb8f191
natashabusnardo <natasha.busnardo@mercadolivre.com>
2022-09-22 17:23:49 -0300

    feat: example03

---
+++ Modulo2Java/Aula01/src/examples/example03.java
@@ -0,0 +1,21 @@
+package examples;
+
+public class example03 {
+    public static void main(String[] args) {
+        int contador = 1;
+
+        while (contador <= 10) {
+            System.out.print(contador + " ");
+            contador++;
+        }
+
+        System.out.println();
+
+        for (contador = 1; contador <= 10; contador++) {
+            System.out.print(contador + " ");
+        }
+
+        for (int a = 1, b = 2;  a > 0  ; a--) {
+        }
+    }
+}
