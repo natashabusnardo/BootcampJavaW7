7cdd62720592442ea6148b981ad7ba64beb27ed5
natashabusnardo <natasha.busnardo@mercadolivre.com>
2022-10-03 10:36:21 -0300

    feat: aula08

---
+++ Modulo3Spring/Aula08/spring03/src/main/java/br/com/dh/spring03/service/VeiculoService.java
@@ -0,0 +1,25 @@
+package br.com.dh.spring03.service;
+
+import br.com.dh.spring03.model.Veiculo;
+import br.com.dh.spring03.repository.VeiculoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VeiculoService implements IVeiculo{
+
+    @Autowired
+    private VeiculoRepo repo;
+
+    @Override
+    public Veiculo getVeiculo(String placa) {
+        return repo.getVeiculo(placa);
+    }
+
+    @Override
+    public List<Veiculo> getAllVeiculos() {
+        return null;
+    }
+}
